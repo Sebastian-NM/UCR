097af025fc9815d73e5a454543d1f0d2b47bfc05
Sebastian-NM <82008773+Sebastian-NM@users.noreply.github.com>
2021-06-15 14:33:20 -0600

    A lot of things

--- src/main/FXMLGraphicInterfaceController.java
+++ src/main/FXMLGraphicInterfaceController.java
@@ -7,7 +7,13 @@ package main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +22,47 @@ import javafx.fxml.Initializable;
  */
 public class FXMLGraphicInterfaceController implements Initializable {
 
+    @FXML
+    private Button btnPut;
+    @FXML
+    private Button btnReplace;
+    @FXML
+    private Button btnRemove;
+    @FXML
+    private Button btnSize;
+    @FXML
+    private Button btnContainsKey;
+    @FXML
+    private Button btnContainsValue;
+    @FXML
+    private Button btnValues;
+    @FXML
+    private Button btnExit;
+    @FXML
+    private Button btnSetKey;
+    @FXML
+    private ComboBox<?> cmbStudents;
+    @FXML
+    private Text txtInfo;
+    @FXML
+    private Text txtTitle;
+    @FXML
+    private Text txtSubHeader1;
+    @FXML
+    private Button btnActionPut;
+    @FXML
+    private Button btnActionReplace;
+    @FXML
+    private Button btnActionRemove;
+    @FXML
+    private Text txtShowValues;
+    @FXML
+    private Button btnActionContainsKey;
+    @FXML
+    private Button btnActionContainsValue;
+    @FXML
+    private Button btnActionSetKey;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +70,64 @@ public class FXMLGraphicInterfaceController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btnPut(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnReplace(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnRemove(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnSize(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnContainsKey(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnContainsValue(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnValues(ActionEvent event) {
+    }
+
+
+    @FXML
+    private void btnExit(ActionEvent event) {
+        Stage stage = (Stage) this.btnExit.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void btnSetKey(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnActionPut(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnActionReplace(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnActionRemove(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnActionContainsValue(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnActionSetKey(ActionEvent event) {
+    }
     
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,12 @@
  */
 package main;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -20,25 +23,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLGraphicInterface.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Hash Map");
+        stage.setResizable(false);
+        stage.show();
     }
 
     /**
